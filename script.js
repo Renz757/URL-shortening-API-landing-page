d1c1652393d74a7bff4cd8afd83d89fe2befa1c7
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2022-07-14 16:09:26 -0400

    Simple validation

--- script.js
+++ script.js
@@ -2,23 +2,34 @@ const button = document.querySelector("#api-button");
 const url = document.querySelector("#api-input");
 const shortLinkList = document.querySelector("#shortLinkList");
 
-button.addEventListener('click', async function testFunc() {
-    const response = await fetch(
-        `https://api.shrtco.de/v2/shorten?url=${url.value}`
-    );
 
-    const data = await response.json();
-    console.log(data.result.short_link)
+
+
+const generateLink = () => {
+    // var valid = /^(ftp|http|https):\/\/[^ "]+$/.test(url);
+    // console.log(valid);
+
+    if (!url.value) {
+        url.style.border = "1px solid red";
+        console.error("Please Enter Valid Url");
+    } else {
+        url.style.border = "none";
+        fetch(`https://api.shrtco.de/v2/shorten?url=${url.value}`)
+            .then(response => response.json())
+            .then(data => console.log(data.result));
+
+        url.value = "";
+    }
+
+
 
     //set input to empty string 
-    
+
     //create element for short link
-    const shortLink = document.createElement('li');
-    shortLink.innerHTML = `<p>${url.value}</p>  <p>${data.result.short_link}</p> `;
-    shortLinkList.appendChild(shortLink);
-    url.value = "";
-});
+    // const shortLink = document.createElement('li');
+    // shortLink.innerHTML = `<p>${url.value}</p>  <p>${data.result.short_link}</p> `;
+    // shortLinkList.appendChild(shortLink);
+    // url.value = "";
+};
 
-
-
-
+button.addEventListener('click', generateLink);
